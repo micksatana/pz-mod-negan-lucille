0d494434afd2eb04d10a266a9dcf90b6c6253eb7
Satana Charuwichitratana <satana.charu@gmail.com>
2025-03-13 22:59:24 +0700

    fix(b42): BarbedWire not dropped after break, allow fixing

--- Contents/mods/NegansBat/42/media/lua/shared/items/OnBreak_Negan.lua
+++ Contents/mods/NegansBat/42/media/lua/shared/items/OnBreak_Negan.lua
@@ -2,6 +2,6 @@ require "shared/Items/OnBreak"
 
 function OnBreak.NegansBat(item, player)
   OnBreak.HandleHandler(item, player, "Base.BaseballBat_Broken", false)
-  OnBreak.HeadHandler(item, player, "Base.Barbwire", true, nil, 1)
+  OnBreak.HeadHandler(item, player, "Base.BarbedWire", true, nil, 0, true)
   OnBreak.GroundHandler(item, player, "Base.Splinters")
 end
